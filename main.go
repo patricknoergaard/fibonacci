f635594ad96450ccc20e39cde63f12e63822c61d
Patrick <patricknoergaard100@gmail.com>
2024-10-18 14:14:04 +0200

    Added comments

--- main.go
+++ main.go
@@ -16,11 +16,14 @@ func main() {
 	start := time.Now()
 
 	for j := 0; j < iterations; j++ {
+		// Start timer and init array
 		startLoop := time.Now()
 		var fib []int
 
+		// Initialize fibonacci sequence
 		a, b := 0, 1
 
+		// Compute fibonacci numbers
 		for b < number_limit {
 			fib = append(fib, b)
 			a, b = b, a+b
@@ -28,16 +31,19 @@ func main() {
 
 		added_nums = 0
 
+		// Add even fibonacci numbers to variable added_nums
 		for i := 1; i < len(fib); i++ {
 			if fib[i]&1 != 1 {
 				added_nums += fib[i]
 			}
 		}
 
+		// Compute average loop duration
 		totalLoopDuration += time.Since(startLoop)
 		dLoop = totalLoopDuration / time.Duration(iterations)
 	}
 
+	// Print result
 	addNums := strconv.Itoa(added_nums)
 	numLim := strconv.Itoa(number_limit)
 	fmt.Println("Even fibinacci numbers under " + numLim + ": " + addNums)
